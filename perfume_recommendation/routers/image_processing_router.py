f0052c63654ded40895d293310fde866b6cc4fdc
kimkinghyeon <modan8199@gmail.com>
2024-11-28 15:10:57 +0900

    - 이미지업로드 파일 수정

--- perfume_recommendation/routers/image_processing_router.py
+++ perfume_recommendation/routers/image_processing_router.py
@@ -1,23 +1,22 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter, UploadFile, File, HTTPException
 from services.image_processing_service import ImageProcessingService
 
 router = APIRouter()
 image_processing_service = ImageProcessingService()
 
-class ImageURLRequest(BaseModel):
-    image_url: str
-
 @router.post("/process-image")
-async def process_image(request: ImageURLRequest):
+async def process_image(file: UploadFile = File(...)):
     """
-    이미지 URL을 처리하여 설명과 느낌을 반환합니다.
+    업로드된 이미지를 처리하여 설명과 느낌을 반환합니다.
     """
     try:
-        # 이미지 데이터 다운로드
-        image_data = image_processing_service.download_image_from_url(request.image_url)
+        # 업로드된 파일의 데이터 읽기
+        image_data = await file.read()
+
         # 이미지 처리
         result = image_processing_service.process_image(image_data)
         return {"message": "이미지 처리 성공", "result": result}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
